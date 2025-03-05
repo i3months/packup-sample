bbd929eb9ce7d8ee979931ea27c2d5e1d778eb6c
i3months <waysoleward01@naver.com>
2025-03-05 20:06:11 +0900

    add:프로젝트 초기 설정

---
+++ src/main/java/com/packup/PackupApplication.java
@@ -0,0 +1,13 @@
+package com.packup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackupApplication.class, args);
+    }
+
+}
